5bf8a24a4605b4305203083c2a787f0a51c63846
Daniel P <darkmagician0609@gmail.com>
2020-03-10 10:20:42 -0600

    Archivo principal, regresa un código HTTP de acuerdo la validación del número de SIM
    Código 200 si existe, código 400 en caso de no existir

---
+++ new/Activation/checkvalue.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'lib.php';
+require 'Connection.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$message = new stdClass();
+
+$connect = new Connection();
+
+$connection = $connect->connect(getenv('SERVERNAME'), getenv('DBNAME'), getenv('USERNAME'), getenv('PASSWORD'));
+
+$validation = new Validation();
+
+$isValid = $validation->validate($connection, $_POST['serial_no']);
+
+if ($isValid) {
+    $message->code = 200;
+    $message->msg = "Existe";
+    echo json_encode($message);
+} else {
+    $message->code = 404;
+    $message->msg = "Número de SIM no válido";
+    echo json_encode($message);
+}
+
+?>
