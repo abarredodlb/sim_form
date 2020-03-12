ad8683687ec1a07c6ba93278f105de40b7ae18c6
Daniel P <darkmagician0609@gmail.com>
2020-03-12 01:26:53 -0600

    Se genera condición para seguir pasos en el formulario de forma adecuada

--- new/Activation/checkvalue.php
+++ new/Activation/checkvalue.php
@@ -8,21 +8,32 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 $message = new stdClass();
 
+session_start();
+
 $connect = new Connection();
 
 $connection = $connect->connect(getenv('DBSERVERNAME'), getenv('DBNAME'), getenv('DBUSERNAME'), getenv('DBPASSWORD'));
 
-$validation = new Validation();
+if (isset($_POST['serial_no'])) {
+    $validation = new Validation();
 
-$isValid = $validation->validate($connection, $_POST['serial_no']);
+    $isValid = $validation->validate($connection, $_POST['serial_no']);
 
-if ($isValid) {
+    if ($isValid) {
+        $message->code = 200;
+        $message->msg = "Existe";
+        echo json_encode($message);
+        $_SESSION['serial_no'] = $_POST['serial_no'];
+    } else {
+        $message->code = 404;
+        $message->msg = "Número de SIM no válido";
+        echo json_encode($message);
+        unset($_SESSION['serial_no']);
+    }
+} elseif (isset($_POST['json'])) {
+    $form_data = json_decode($_POST['json']);
     $message->code = 200;
-    $message->msg = "Existe";
-    echo json_encode($message);
-} else {
-    $message->code = 404;
-    $message->msg = "Número de SIM no válido";
+    $message->msg = "Good job";
     echo json_encode($message);
 }
 
